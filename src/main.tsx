3d9e548d9bfdcc3575c95fcd0efcc506bf11370f
Guirdo <produccionesaldo@gmail.com>
2023-02-06 19:11:59 -0600

    Update main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './compenents/App'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </BrowserRouter>
