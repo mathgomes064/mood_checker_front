9b7b6e196ce67fbda3ff330fcf276ce92dae38a4
Guirdo <produccionesaldo@gmail.com>
2023-02-07 13:05:30 -0600

    Add Compose component

---
+++ src/compenents/HomePage/Compose.tsx
@@ -0,0 +1,30 @@
+import { Avatar, Flex, Box, Textarea, Button } from '@chakra-ui/react'
+
+function Compose () {
+  return (
+    <Flex
+      gap="2"
+      padding="3"
+    >
+      <Box>
+        <Avatar />
+      </Box>
+
+      <Flex
+        gap="2"
+        flexDirection="column"
+        flexGrow="1"
+      >
+        <Textarea />
+
+        <Button
+          colorScheme="green"
+        >
+          Send
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Compose
