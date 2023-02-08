3d635e699ee624327c580d071e0e5e7dd7281eec
BeaBea <pcreis.beatriz@gmail.com>
2023-02-08 12:18:24 -0300

    :sparkles: Add new routes

--- src/compenents/App/App.tsx
+++ src/compenents/App/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../HomePage'
+import LandingPage from '../LandingPage'
+import SignUp from '../SignUp/SignUp'
 
 function App () {
   return (
     <Routes>
+      <Route path='/' element={<LandingPage />} />
       <Route path='/home' element={<HomePage />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   )
 }
