16721707c64aafa9b63bbc714e64ff59ee8e32eb
BeaBea <pcreis.beatriz@gmail.com>
2023-02-09 12:28:44 -0300

    ➕ Add baseUrl to axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-//   baseURL: 'https://moodchecker.com',
+  baseURL: 'https://moodcheckerback-production.up.railway.app/',
   headers: {
     'Content-Type': 'application/json'
   }
